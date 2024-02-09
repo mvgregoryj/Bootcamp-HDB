1a91463967611c6bee46802008bfad92b49cd7c9
Gregory <mvgregoryj@gmail.com>
2024-02-09 11:36:17 -0400

    Update polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -3,4 +3,4 @@ import * as process from 'process';
 
 (window as any).global = window;
 (window as any).global.Buffer = Buffer;
-(window as any).process = process;
+(window as any).process = { env: { } };
